898542a0d1cc0df03d0fbb0c9f2c7a5f94066d9e
Criciun Vlad <criciunvlad19@gmail.com>
2023-12-07 16:23:21 +0200

    Added the implementation of Zoo Service by using OOP

---
+++ src/main/java/org/VladQA/zooclubservice/Animal.java
@@ -0,0 +1,15 @@
+package org.VladQA.zooclubservice;
+
+public abstract class  Animal implements  AnimalInterface{
+    private String name;
+
+    public Animal(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public abstract void eat();
+
+}
+
---
+++ src/main/java/org/VladQA/zooclubservice/Cat.java
@@ -0,0 +1,17 @@
+package org.VladQA.zooclubservice;
+
+public class Cat extends Animal implements AnimalInterface{
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Cat " + getName() + " says: Meow!");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is eating KitKat.");
+    }
+}
